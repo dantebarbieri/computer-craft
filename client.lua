0cd94bc86495c8c15c5480d5b96bfd323f167c1b
dantebarbieri <dantevbarbieri@gmail.com>
2025-01-26 20:47:02 -0600

    Add auto fueling to client

--- client.lua
+++ client.lua
@@ -19,6 +19,22 @@ end
 modem.open(CLIENT_RECV)
 modem.open(MY_RECV)
 
+modem.transmit(LOGS_RECV, MY_RECV, "client started")
+
+local function refuel()
+    if turtle.getFuelLevel() > 0 then
+        return
+    end
+
+    for i = 1, 16 do -- loop through the slots
+        turtle.select(i) -- change to the slot
+        if turtle.refuel(0) then -- if it's valid fuel
+            turtle.refuel(1)     -- refuel using one
+            return
+        end
+      end
+end
+
 repeat
     local event, modemSide, senderChannel, replyChannel, message, senderDistance = os.pullEvent("modem_message")
     local response = "ack: " .. message .. " from " .. replyChannel
@@ -27,17 +43,25 @@ repeat
 
     local log = nil
     if message == "forward" then
+        refuel()
         log = turtle.forward() and "moved forward" or "could not move forward"
     elseif message == "back" then
+        refuel()
         log = turtle.back() and "moved back" or "could not move back"
     elseif message == "up" then
+        refuel()
         log = turtle.up() and "moved up" or "could not move up"
     elseif message == "down" then
+        refuel()
         log = turtle.down() and "moved down" or "could not move down"
     elseif message == "left" then
+        refuel()
         log = turtle.turnLeft() and "turned left" or "could not turn left"
     elseif message == "right" then
+        refuel()
         log = turtle.turnRight() and "turned right" or "could not turn right"
+    elseif message == "dig" then
+        log = turtle.dig() and "dug" or "could not dig"
     end
 
     if log then
@@ -45,6 +69,8 @@ repeat
     end
 
 until message == EXIT_COMMAND
+
+modem.transmit(LOGS_RECV, MY_RECV, "client ended")
 
 modem.close(MY_RECV)
 modem.close(CLIENT_RECV)
